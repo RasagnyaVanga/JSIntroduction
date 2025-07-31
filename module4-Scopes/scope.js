ad1cfc70edfe97be9663d6f5c65acce8e1c59904
Rasagnya Vanga <rasagnya.vanga@beautifulcode.in>
2025-07-31 15:51:25 +0530

    few changes

--- module4-Scopes/scope.js
+++ module4-Scopes/scope.js
@@ -4,7 +4,7 @@ function check() {
 
     var var_f = 100; //var_f variable with functional scope - only accessed within this function
     console.log("Accessing global from function: " + var_g);
-    
+
     if (var_g > 10) {
         let let_b = 100; //let with block scope - only accessed in this block
         const const_b = 100; //const with block scope - only accessed in this block
@@ -23,6 +23,8 @@ function check() {
     }
 }
 check();
+
+//Accessing function variables out of function
 try {
     console.log("Accessing function scope variable outside function: " + var_f);
 }
