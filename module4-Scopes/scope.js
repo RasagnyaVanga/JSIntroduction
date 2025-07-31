54be01175e3c20213290e2440828b9c5fdc0f0f3
Rasagnya Vanga <rasagnya.vanga@beautifulcode.in>
2025-07-31 15:50:15 +0530

    few changes

--- module4-Scopes/scope.js
+++ module4-Scopes/scope.js
@@ -1,8 +1,10 @@
 var var_g = 100; //var_g -- variable with global scope
 
 function check() {
+
     var var_f = 100; //var_f variable with functional scope - only accessed within this function
     console.log("Accessing global from function: " + var_g);
+    
     if (var_g > 10) {
         let let_b = 100; //let with block scope - only accessed in this block
         const const_b = 100; //const with block scope - only accessed in this block
@@ -11,18 +13,13 @@ function check() {
     }
     //we can access var_b as var do not have block scope only till functional scope
     console.log(`Accessing variable declared with var in block from out of block: ${var_b}`);
+
+    //Accessing block variables out of block 
     try {
         console.log("Accessing block scope variable(declared with let) outside block: " + let_b);
     }
     catch (error) {
         console.log("Accessing block scope variable(with let) outside block: " + error.message);
-    }
-
-    try {
-        console.log("Accessing block scope variable(declared with const) outside block: " + const_b);
-    }
-    catch (error) {
-        console.log("Accessing block scope(with const) variable outside block: " + error.message);
     }
 }
 check();
