ae01fd06053c6504bd547ce1903ef1089b094289
Rasagnya Vanga <rasagnya.vanga@beautifulcode.in>
2025-08-07 10:52:01 +0530

    changed few functions in mathUtils.mjs

--- module11-modules/app.mjs
+++ module11-modules/app.mjs
@@ -1,8 +1,6 @@
-import sayHello from "./mathUtils.mjs"; //importing the sayHellofunction from other module js file (exported through default)
-import { sayGoodBye } from "./mathUtils.mjs"; //importing the the sayGoodBye function from other module js file(exported through inline)
-function greetings(){
-    sayHello();
-    console.log("How are you??");
-    sayGoodBye();
-}
-greetings();
+import divide from "./mathUtils.mjs"; //importing the divide function from other module js file (exported through default)
+import { add, subtract, multiply} from "./mathUtils.mjs"; //importing multiple functions from other module js file(exported through inline)
+console.log(add(10,20));
+console.log(subtract(20,10));
+console.log(multiply(10,20));
+console.log(divide(10,20));
--- module11-modules/mathUtils.mjs
+++ module11-modules/mathUtils.mjs
@@ -1,8 +1,16 @@
-export const name="Stefan"; //inline export -- can export any number of objects, functions and variables through this method.
-function sayHello(){
-    console.log("Hello guyss!!...");
+export const num1=13; //inline export -- can export any number of objects, functions and variables through this method.
+export const num2=15;
+function add(num1,num2){
+    return num1+num2;
 }
-export function sayGoodBye(){ //inline export
-    console.log("Bye bye see you again..!!");
+function subtract(num1,num2){
+    return num1-num2;
 }
-export default sayHello; //exporting the function through default //only one default export per file. export one main thing
+export function multiply(num1,num2){ //named export - inline export
+    return num1*num2;
+}
+function divide(num1,num2){ 
+    return num1/num2;
+}
+export {add,subtract}; //named export - group at once 
+export default divide; //exporting the function through default //only one default export per file. export one main thing
