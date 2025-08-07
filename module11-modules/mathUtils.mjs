ba17a07eb5581910fd4b089fc4aa5a5dd6bf71f0
Rasagnya Vanga <rasagnya.vanga@beautifulcode.in>
2025-08-07 10:40:09 +0530

    changes made in importing and exporting the modules

--- module11-modules/mathUtils.mjs
+++ module11-modules/mathUtils.mjs
@@ -1,7 +1,8 @@
+export const name="Stefan"; //inline export -- can export any number of objects, functions and variables through this method.
 function sayHello(){
     console.log("Hello guyss!!...");
 }
-export function sayGoodBye(){ //exporting this function through inline
+export function sayGoodBye(){ //inline export
     console.log("Bye bye see you again..!!");
 }
-export default sayHello; //exporting the function through default
+export default sayHello; //exporting the function through default //only one default export per file. export one main thing
